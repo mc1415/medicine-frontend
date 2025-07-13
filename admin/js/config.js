321d46ccce67bbe8285d02736bbb7d7d6bd73da5
mc1415 <155986849+mc1415@users.noreply.github.com>
2025-07-13 22:27:02 +0700

    Update config.js

--- admin/js/config.js
+++ admin/js/config.js
@@ -2,9 +2,9 @@
 
 const AppConfig = {
     // --- Add these new lines at the top ---
-    SUPABASE_URL:"https://triozmzezdeonfwurywn.supabase.co",
-    SUPABASE_ANON_KEY:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyaW96bXplemRlb25md3VyeXduIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0OTc5NDA4NCwiZXhwIjoyMDY1MzcwMDg0fQ.7uVg3k_JI0rhgigY2TLCDMfSaEwrAMwKEJ8zjkwLDUs",
+    SUPABASE_URL:"https://xstowaqykzyejegyqurd.supabase.co",
+    SUPABASE_ANON_KEY:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzdG93YXF5a3p5ZWplZ3lxdXJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0MTUxMzcsImV4cCI6MjA2Nzk5MTEzN30.9wB2TVBpMvuxxB1swg0KmY44W9lcrJI6d-tTUoG0Nw4",
 
     // --- Your existing currency configuration ---
-    BASE_CURRENCY: 'THB',
+    BASE_CURRENCY: 'USD',
 };
